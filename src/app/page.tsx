0f8e798c54af19eaeaa7c8b7fe8730fe38cbc7b3
taniabmguedes@gmail.com <taniabmguedes@gmail.com>
2025-11-26 14:51:20 +0000

    first grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,50 @@
+import { Container } from "@/components/Container";
+import { Header } from "@/components/Header";
+import { PostCoverImage } from "@/components/PostCoverImage";
+import { PostHeading } from "@/components/PostHeading";
 import { PostList } from "@/components/PostList";
 import { SpinLoader } from "@/components/SpinLoader";
 import { Suspense } from "react";
 
 export default async function HomePage() {
   return (
-    <div>
-      <header>
-        <h1 className="text-6xl font-bold text-center py-8">HEADER</h1>
-      </header>
+    <Container>
+      <Header />
+      <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
+        <PostCoverImage
+          linkProps={{
+            href: "/post/asdfasdf",
+          }}
+          imageProps={{
+            width: 1200,
+            height: 720,
+            src: "/images/bryen_0.png",
+            alt: "Alt da imagem",
+            priority: true,
+          }}
+        />
+        <div className="flex flex-col gap-4 sm:justify-center">
+          <time
+            className="text-slate-600 block text-sm/tight"
+            dateTime="26-11-2025 13:09"
+          >
+            26-11-2025
+          </time>
+          <PostHeading as="h1" url="#">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In velit
+          </PostHeading>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In velit
+          dolorem est dolor porro, doloribus neque, quidem mollitia doloremque,
+          ad perspiciatis fugiat. Rerum, vel ex? Impedit ullam harum blanditiis
+          mollitia?
+        </div>
+      </section>
       <Suspense fallback={<SpinLoader />}>
         <PostList />
       </Suspense>
       <footer>
         <p className="text-6xl font-bold text-center py-8">FOOTER</p>
       </footer>
-    </div>
+    </Container>
   );
 }
