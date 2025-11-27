b1b3cdbc02b342f11ff5dc375dac3dbef6fa5af5
taniabmguedes@gmail.com <taniabmguedes@gmail.com>
2025-11-27 11:45:49 +0000

    error page and client and server component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,25 +1,12 @@
+import ErrorMesssage from "@/components/ErrorMessage";
 import clsx from "clsx";
 
 export default function NotFoundPage() {
   return (
-    <>
-      <title>Page Not Found</title>
-      <div
-        className={clsx(
-          "'min-h-[320px] ",
-          "bg-slate-900",
-          "text-slate-100",
-          "mb-16",
-          "p-8",
-          "rounded-xl",
-          "flex items-center justify-center"
-        )}
-      >
-        <div>
-          <h1 className="text-7xl/tight mb-4 font-extrabold">ERROR 404</h1>
-          <p>Erro 404 - This page does not exist in this website</p>
-        </div>
-      </div>
-    </>
+    <ErrorMesssage
+      pageTitle="Page not found"
+      contentTitle="404"
+      content="Erro 404 - This page does not exists in this website"
+    />
   );
 }
---
+++ src/components/ClientComponent/index.tsx
@@ -0,0 +1,6 @@
+"useClient";
+
+export function ClientComponent({ children }: { children: React.ReactNode }) {
+  console.log("ClientComponent");
+  return <div>ClientComponent {children}</div>;
+}
