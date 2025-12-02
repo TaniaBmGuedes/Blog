3b3007b2c4f70b2d7e8385deea0ed5f8931d8518
taniabmguedes@gmail.com <taniabmguedes@gmail.com>
2025-12-02 13:55:46 +0000

    image uploader

---
+++ src/components/admin/ImageUploader/index.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { Button } from "@/components/Button";
+import { IMAGE_UPLOAD_MAX_SIZE } from "@/lib/constants";
+import { ImageUpIcon } from "lucide-react";
+import { useRef } from "react";
+import { toast } from "react-toastify";
+
+export default function ImageUploader() {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  function handleChooseFile() {
+    if (!fileInputRef.current) return;
+
+    fileInputRef.current.click();
+  }
+
+  function handleChange() {
+    if (!fileInputRef.current) return;
+
+    const fileInput = fileInputRef.current;
+    const file = fileInput?.files?.[0];
+
+    if (!file) return;
+
+    if (file.size > IMAGE_UPLOAD_MAX_SIZE) {
+      const readableMaxSize = IMAGE_UPLOAD_MAX_SIZE / 1024;
+      toast.error(`Image to big. Max.: ${readableMaxSize}KB.`);
+
+      fileInput.value = "";
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    // TODO: Criar a action para upload de arquivo
+
+    fileInput.value = "";
+  }
+
+  return (
+    <div className="flex flex-col gap-2 py-4">
+      <Button onClick={handleChooseFile} type="button" className="self-start">
+        <ImageUpIcon />
+        Send an image
+      </Button>
+
+      <input
+        ref={fileInputRef}
+        onChange={handleChange}
+        className="hidden"
+        name="file"
+        type="file"
+        accept="image/*"
+      />
+    </div>
+  );
+}
--- src/components/admin/ManagePostForm/index.tsx
+++ src/components/admin/ManagePostForm/index.tsx
@@ -1,10 +1,16 @@
+"use client";
+
 import { Button } from "@/components/Button";
 import { InputCheckbox } from "@/components/InputChecbox";
 import { InputText } from "@/components/InputText";
+import { MarkdownEditor } from "@/components/MarkdownEditor";
+import { useState } from "react";
+import ImageUploader from "../ImageUploader";
 
 export const dynamic = "force-dynamic";
 
-export default async function ManagePostForm() {
+export default function ManagePostForm() {
+  const [contentValue, setContentValue] = useState("");
   return (
     <form action="" className="mb-16">
       <div className="flex flex-col gap-6">
@@ -16,28 +22,14 @@ export default async function ManagePostForm() {
         <InputText labelText="Sobrenome" placeholder="Digite seu sobrenome" />
 
         <InputCheckbox labelText="Sobrenome" />
+        <ImageUploader />
 
-        <InputText
-          disabled
-          labelText="Sobrenome"
-          placeholder="Digite seu sobrenome"
-          defaultValue="Olá mundo"
-        />
-        <InputText
-          disabled
-          labelText="Sobrenome"
-          placeholder="Digite seu sobrenome"
-        />
-        <InputText
-          labelText="Sobrenome"
-          placeholder="Digite seu sobrenome"
-          readOnly
-        />
-        <InputText
-          labelText="Sobrenome"
-          placeholder="Digite seu sobrenome"
-          defaultValue="Olá mundo"
-          readOnly
+        <MarkdownEditor
+          labelText="Conteúdo"
+          disabled={false}
+          textAreaName="content"
+          value={contentValue}
+          setValue={setContentValue}
         />
 
         <div className="mt-4">
